4308b108dc3cbc2ca4d7052098c04f64e9530cd6
Akash Verma <akash.911verma@gmail.com>
2023-05-10 17:44:30 +0530

    first commit

---
+++ backend/djreact/djreact/urls.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path,include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('articles/', include('articles.api.urls')),
+    path('hackathons/', include('hackathons.urls')),
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
